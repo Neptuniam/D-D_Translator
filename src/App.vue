7115aa7ac9d7d22bf75124522f0bf33646e09428
Neptuniam <ljones1997@hotmail.com>
2019-12-24 13:56:43 -0500

    Google Translation setup

--- src/App.vue
+++ src/App.vue
@@ -1,28 +1,40 @@
 <template>
-  <div id="app">
-    <img alt="Vue logo" src="./assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
-  </div>
+<div>
+    <div class="center-xs Intro">
+        <h1> Josh's D&D Language Translator</h1>
+        <h3>Built By: Liam Jones</h3>
+    </div>
+
+    <Forwards />
+</div>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
+import Forwards from './components/Forwards.vue'
 
 export default {
   name: 'app',
   components: {
-    HelloWorld
+    Forwards
   }
 }
 </script>
 
 <style>
-#app {
-  font-family: 'Avenir', Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
-}
+    #app {
+      font-family: 'Avenir', Helvetica, Arial, sans-serif;
+      -webkit-font-smoothing: antialiased;
+      -moz-osx-font-smoothing: grayscale;
+      text-align: center;
+      color: #2c3e50;
+      margin-top: 60px;
+    }
+
+    .Intro {
+        margin: 50px 0px 100px 0px;
+    }
+
+    .fullWidth {
+        width: 100% !important;
+    }
 </style>
